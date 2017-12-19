4a79826412703c6c96c64915684963a4dfcce0e1
muriungialvin <alvin.muriungi@strathmore.edu>
2017-12-19 13:28:11 +0300

    first commit

---
+++ app/src/main/java/com/androidtutorialpoint/androidimageslider/MainActivity.java
@@ -0,0 +1,52 @@
+package com.androidtutorialpoint.androidimageslider;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class MainActivity extends AppCompatActivity {
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] SLIDESHOW= {R.drawable.strath_logo,R.drawable.college_vote,R.drawable.screwy_students,R.drawable.voting};
+    private ArrayList<Integer> SLIDESHOWArray = new ArrayList<Integer>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init() {
+        for(int i=0;i<SLIDESHOW.length;i++)
+            SLIDESHOWArray.add(SLIDESHOW[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyAdapter(MainActivity.this,SLIDESHOWArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == SLIDESHOW.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
+    }
+
+}
